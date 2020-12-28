dc9c2024029206e8e3a695949702aec6f3c88fba
priyadarshu <priyadarshinichettiar@gmail.com>
2020-12-29 00:39:28 +0530

    Added Tab Bar Code

--- lib/main.dart
+++ lib/main.dart
@@ -5,56 +5,36 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final List icondetails = [
-    {
-      "title": "Chat",
-      "icon": Icons.mark_chat_unread_rounded,
-      "tooltip": "",
-      "color": Colors.orangeAccent,
-      "alignment": Alignment.centerLeft
-    },
-    {
-      "title": "Email",
-      "icon": Icons.email,
-      "tooltip": "",
-      "color": Colors.blueAccent,
-      "alignment": Alignment.center
-    },
-    {
-      "title": "Social",
-      "icon": Icons.add_box_rounded,
-      "tooltip": "",
-      "color": Colors.redAccent,
-      "alignment": Alignment.centerRight
-    }
-  ];
-
   @override
   Widget build(BuildContext context) {
-    /* var actions2 = [
-      for (int i = 0; i < icondetails.length; i++)
-        new IconButton(
-          icon: icondetails[i].icon,
-          tooltip: "",
-          color: Colors.orangeAccent,
-          alignment: Alignment.centerLeft,
-          onPressed: () {},
-        ),
-    ];
-    var actions22 = actions2;*/
+    /* final _tabs = <Tab>[
+      Tab(icon: Icon(Icons.mark_chat_unread_rounded), text='Chat'),
+      Tab(icon: Icon(Icons.email), text='Email'),
+      Tab(icon: Icon(Icons.add_box_rounded), text='Social'),
+    ];*/
 
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Chat App UI",
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(""),
+        debugShowCheckedModeBanner: false,
+        title: "ChatApp",
+        theme: ThemeData(
+          primaryColor: Colors.white,
         ),
-        body: Container(),
-      ),
-    );
+        home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+              appBar: AppBar(
+                  leading: null,
+                  bottom: TabBar(
+                    labelPadding: EdgeInsets.all(0),
+                    tabs: [
+                      Tab(
+                        icon: Icon(Icons.mark_chat_unread_rounded),
+                        text: 'Chat',
+                      ),
+                      Tab(icon: Icon(Icons.email), text: 'Email'),
+                      Tab(icon: Icon(Icons.add_box_rounded), text: 'Social'),
+                    ],
+                  ))),
+        ));
   }
 }
